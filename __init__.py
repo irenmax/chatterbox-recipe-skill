f3ae07022f98f713987c4220c8117bd1367c1865
Max <e1634015@student.tuwien.ac.at>
2021-05-07 19:44:50 +0200

    tryout with other voc file

--- __init__.py
+++ __init__.py
@@ -10,6 +10,9 @@ class HelloWorldSkill(ChatterboxSkill):
     def handle_intent_name2Intent(self, message):
         self.speak('hello world')
 
+    @intent_handler(IntentBuilder("nextLine").require('nextline'))
+    def handle_next_line(self, message):
+      self.speak('This is the next line')
 
 def create_skill():
     return HelloWorldSkill()
