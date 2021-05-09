668ed4aa15a9c1431a307ece5f0a1f2401d0e0c6
Max <e1634015@student.tuwien.ac.at>
2021-05-09 20:16:01 +0200

    recipe class

---
+++ Recipe.py
@@ -0,0 +1,19 @@
+import os 
+class Recipe:
+  stepCount = 0
+  def __init__(self, recipeName):
+    recipe = open(recipeName, "r")
+    content = recipe.read()
+    content = content.replace("\n", "")
+    Recipe.stepList = filter(None, content.split("- [ ] "))
+    recipe.close()
+  
+  def getLine(self, index):
+    return Recipe.stepList[index]
+
+  def getNextStep(self):
+    step = Recipe.stepList[Recipe.stepCount]
+    Recipe.stepCount = Recipe.stepCount + 1
+    return step
+
+
--- __init__.py
+++ __init__.py
@@ -1,18 +1,22 @@
 from adapt.intent import IntentBuilder
 from chatterbox.skills.core import ChatterboxSkill
 from chatterbox.skills.core import intent_handler
-
+from Recipe import Recipe
 
 class HelloWorldSkill(ChatterboxSkill):
-
+    
     @intent_handler(IntentBuilder("helloWorld").
                     require('hello'))
     def handle_intent_name2Intent(self, message):
         self.speak('hello world')
 
-    @intent_handler(IntentBuilder("nextLine").require('nextline'))
-    def handle_next_line(self, message):
-      self.speak('This is the next line')
+    @intent_handler(IntentBuilder("getRecipe").require('getRecipe'))
+    def handle_get_Recipe(self, message):
+        self.Recipe = Recipe("recipe_1.txt")
+
+    @intent_handler(IntentBuilder("nextStep").require('nextStep'))
+    def handle_next_step(self, message):
+      self.speak(self.Recipe.getNextStep)
 
 def create_skill():
     return HelloWorldSkill()
