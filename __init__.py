e5b17192d61420d4dcedff47f14342b2332653a5
Max <e1634015@student.tuwien.ac.at>
2021-05-10 20:19:20 +0200

    removed class structure

--- __init__.py
+++ __init__.py
@@ -2,27 +2,8 @@ from adapt.intent import IntentBuilder
 from chatterbox.skills.core import ChatterboxSkill
 from chatterbox.skills.core import intent_handler
 
-class Recipe:
-  stepCount = 0
-  def __init__(self, recipeName):
-    recipe = open(recipeName, "r")
-    content = recipe.read()
-    content = content.replace("\n", "")
-    Recipe.stepList = filter(None, content.split("- [ ] "))
-    recipe.close()
-  
-  def getLine(self, index):
-    return Recipe.stepList[index]
-
-  def getNextStep(self):
-    step = Recipe.stepList[Recipe.stepCount]
-    Recipe.stepCount = Recipe.stepCount + 1
-    return step
-
-
 
 class RecipeSkill(ChatterboxSkill):
-
     
     @intent_handler(IntentBuilder("helloWorld").
                     require('hello'))
@@ -31,11 +12,19 @@ class RecipeSkill(ChatterboxSkill):
 
     @intent_handler(IntentBuilder("getRecipe").require('getRecipe'))
     def handle_getRecipe(self, message):
-        self.recipe = Recipe("recipe_1.txt")
+       # self.recipe = Recipe("recipe_1.txt")
+       with self.file_system.open("recipe_1.txt", "w") as recipe:
+           content = recipe.read()
+           content = content.replace("\n", "")
+           self.stepList = filter(None, content.split("- [ ] "))
+           self.stepCount = 0
+           recipe.close()
 
     @intent_handler(IntentBuilder("nextStep").require('nextStep'))
     def handle_next_step(self, message):
-        self.speak(self.recipe.getNextStep())
+        step = self.stepList[self.stepCount]
+        self.stepCount = self.stepCount + 1
+        self.speak(step)
 
 def create_skill():
     return RecipeSkill()
