0a568a55538f56f65ac4d0716a6c2ba71dec83b9
Max <e1634015@student.tuwien.ac.at>
2021-05-10 20:29:09 +0200

    read file

--- __init__.py
+++ __init__.py
@@ -14,18 +14,21 @@ class RecipeSkill(ChatterboxSkill):
     def handle_getRecipe(self, message):
        self.speak("ok, getting recipe")
        # self.recipe = Recipe("recipe_1.txt")
-       with self.file_system.open("recipe_1.txt", "w") as recipe:
-           content = recipe.read()
-           content = content.replace("\n", "")
-           self.stepList = filter(None, content.split("- [ ] "))
-           self.stepCount = 0
-           recipe.close()
+       self.stepList = self.read_recipe("recipe_1.txt")
+
 
     @intent_handler(IntentBuilder("nextStep").require('nextStep'))
     def handle_next_step(self, message):
         step = self.stepList[self.stepCount]
         self.stepCount = self.stepCount + 1
         self.speak(step)
+
+
+    def read_recipe(self, file_name):
+        with self.file_system.open(file_name, "r") as recipe:
+            content = recipe.read()
+            content = content.replace("\n", "")
+            return filter(None, content.split("- [ ] "))
 
 def create_skill():
     return RecipeSkill()
