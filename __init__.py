639892f1008c9921f202c183c78b21070682f6b1
Max <e1634015@student.tuwien.ac.at>
2021-05-11 17:39:37 +0200

    restructured init

--- __init__.py
+++ __init__.py
@@ -1,39 +1,30 @@
 from adapt.intent import IntentBuilder
 from chatterbox.skills.core import ChatterboxSkill
 from chatterbox.skills.core import intent_handler
-import urllib3
-
+import requests
 
 class RecipeSkill(ChatterboxSkill):
-
-
     def __init__(self):
         super().__init__()
         self.stepCount = 0
         self.stepList = []
     
-    @intent_handler(IntentBuilder("helloWorld").
-                    require('hello'))
-    def handle_intent_name2Intent(self, message):
+    @intent_handler(IntentBuilder('helloWorld').require('hello'))
+    def handle_intent_helloWorld(self, message):
         self.speak('hello world')
 
-    @intent_handler(IntentBuilder("getRecipe").require('getRecipe'))
+    @intent_handler(IntentBuilder('getRecipe').require('getRecipe'))
     def handle_getRecipe(self, message):
-       http = urllib3.PoolManager()
-       response = http.request('GET', 'http://1e2ba836c4e1.ngrok.io/recipe_1.txt')
-       status = response.status
-       content = response.data
-       self.speak(status)
-       self.speak(content)
-
-
-    @intent_handler(IntentBuilder("nextStep").require('nextStep'))
-    def handle_next_step(self, message):
+       response = requests.get('http://1e2ba836c4e1.ngrok.io/recipe_1.txt')
+       self.speak(response.status_code)
+       self.speak(response.content)
+       self.stepList =["One", "Two", "Three"]
+       
+    @intent_handler(IntentBuilder('nextStep').require('nextStep'))
+    def handle_nextStep(self, message):
         step = self.stepList[self.stepCount]
         self.stepCount = self.stepCount + 1
         self.speak(step)
-
-
 
 def create_skill():
     return RecipeSkill()
