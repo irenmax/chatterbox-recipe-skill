2fa1b84e95e70f7b255dc493619d08d1286997f5
Max <e1634015@student.tuwien.ac.at>
2021-05-11 19:51:04 +0200

    another approach

--- __init__.py
+++ __init__.py
@@ -1,11 +1,32 @@
 from adapt.intent import IntentBuilder
 from chatterbox.skills.core import ChatterboxSkill
 from chatterbox.skills.core import intent_handler
-from recipe import Recipe
+import requests
+
+
+def loadRecipe(stepCounter, stepList, recipeName):
+    stepCounter = 0
+    response = requests.get('http://1e2ba836c4e1.ngrok.io/recipe_1.txt')
+    content = response.content
+    content = content.replace("\n", "")
+    stepList = filter(None, content.split("- [ ] "))
+    return stepCounter, stepList
+  
+def getNextStep(stepCounter, stepList):
+    if stepCounter < len(stepList):
+        step = stepList[stepCounter]
+        stepCounter = stepCounter + 1
+        return step
+    else:
+        return 'No more steps'
+
+
 
 class RecipeSkill(ChatterboxSkill):
     def __init__(self):
         super().__init__()
+        self.stepCounter = 0
+        self.stepList = []
 
     
     @intent_handler(IntentBuilder('helloWorld').require('hello'))
@@ -14,13 +35,12 @@ class RecipeSkill(ChatterboxSkill):
 
     @intent_handler(IntentBuilder('getRecipe').require('getRecipe'))
     def handle_getRecipe(self, message):
-        self.recipe1 = Recipe()
-        self.speak('Okay')
+        self.stepCounter, self.stepList = loadRecipe(self.stepCounter, self.stepList, 'recipe1')
        
     @intent_handler(IntentBuilder('nextStep').require('nextStep'))
     def handle_nextStep(self, message):
         self.speak('Okay')
-        step = self.recipe1.getNextStep()
+        step = getNextStep(self.stepCounter, self.stepList)
         self.speak(step)
 
 def create_skill():
